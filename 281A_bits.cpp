3ab00f44982c4fbcca3f8a7b306b4821ce38a647
tsurubr <protsuru@gmail.com>
2023-08-02 09:00:28 -0300

    optime for loop

--- 281A_bits.cpp
+++ 281A_bits.cpp
@@ -1,25 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main() {
     int n = 0;
     cin >> n;
     int result = 0;
     
-for(int i = 0; i < n; i++){
-    string value;
-    cin >> value;
-	
-	for(int j = 0; j <=3; j++){
-    	if(value[j] == '+'){
-        	result++;
-			break;
-    	}else if(value [j] == '-'){
-        	result--;
-			break;
-    	}
-	}
-}
+    for (int i = 0; i < n; i++) {
+        string value;
+        cin >> value;
+        
+        // Procurar por cada caractere da string
+        for (char c : value) {
+            if (c == '+') {
+                result++;
+                break;
+            } else if (c == '-') {
+                result--;
+                break;
+            }
+        }
+    }
+    
     cout << result << endl;
     return 0;
 }
