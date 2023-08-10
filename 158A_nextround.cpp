555192962bd0b92b3bb7edb0df0eeaae81b49a90
tsurubr <protsuru@gmail.com>
2023-08-10 16:48:12 -0300

    final solution

--- 158A_nextround.cpp
+++ 158A_nextround.cpp
@@ -6,12 +6,16 @@ int n = 0;
 int k = 0;
 int a = 0;
 int result = 0;
+vector <int> numeros;
 
 cin >> n >> k;
 
-for (int i = 0; i < n; i++){
+for (int i = 0; i < n; i++){ 
     cin >> a;
-    if (a > k){
+    numeros.push_back(a);
+}
+for (int i = 0; i < n; i++){ 
+    if(numeros[i] >= numeros[k-1] && numeros[i] > 0){
         result++;
     }
 }
