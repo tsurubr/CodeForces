cb7b4344d5501321a6d21d4403ecbebd6ffbcff2
tsurubr <protsuru@gmail.com>
2023-08-14 02:10:20 -0300

    accepted submission

--- 271A_year.cpp
+++ 271A_year.cpp
@@ -5,19 +5,23 @@ int main(){
     int resultado = 0;
     int matriz[5][5];
     for(int i = 0; i < 5; i++){
-    resultado = 0;
-        cin >> matriz[i][i];
-        for(int j = 1; j < 5; j++){
+        for(int j = 0; j < 5; j++){
             cin >> matriz[i][j];
+
             if(matriz[i][j] != 0){
-                if(i && j != 2){
-                    resultado = abs(2 - i);
-                    resultado += abs(2-j);
-                    cout << resultado;
+                if((i || j) != 2){
+                    if (i != 2){
+                        resultado = abs(2 - i);
+                    }
+                    if(j != 2){
+                        resultado += abs(2 - j);                       
+                    }
+                    cout << resultado;                
+                }else{
+                    cout << 0;
                 }
             }
         }
-
     }
 return 0;   
 }
